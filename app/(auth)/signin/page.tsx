c6762fbb60b01babf3c89261c1da045f9fe127b3
AungNyanHtet18 <aungnyanh81@gmail.com>
2026-01-24 00:35:38 +0630

    create login form layout

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,5 +1,14 @@
+import { AuthInput } from "@/components/widgets/authinput";
+import { Key, User } from "lucide-react";
+
 export default function Page() {
      return (
-        <h1>Sign In</h1>
+      <div className="w-1/3 flex justify-center items-center">
+         <div className="flex flex-col gap-3">
+          <h3 className="text-[25px] text-indigo-600 font-medium">USER SIGNIN</h3>
+            <AuthInput icon={<Key/>} label="Enter your email"></AuthInput>
+            <AuthInput icon={<Key/>} label="Enter your password"></AuthInput>
+          </div>
+       </div>
      )
 }
--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,18 +1,14 @@
-import { Key, User } from "lucide-react";
+import { AuthInput } from "@/components/widgets/authinput";
+import { Key, Lock, User } from "lucide-react";
 
 export default function Page() {
      return (
        <div className="w-1/3 flex justify-center items-center">
-         <div className="flex flex-col gap-2">
-          <h3 className="text-[25px] text-indigo-600 text-center font-medium">USER LOGIN</h3>
-          <div className="flex items-center border rounded-md py-2 px-2">
-            <User size={20} className="text-indigo-200"/>
-            <input type="text" className="outline-none"/>
-
-
-          </div>
-
-
+         <div className="flex flex-col gap-3">
+          <h3 className="text-[25px] text-indigo-600 font-medium">USER SIGNUP</h3>
+            <AuthInput icon={<Key/>} label="Enter your name"></AuthInput>
+            <AuthInput icon={<Key/>} label="Enter your email" type="email"></AuthInput>
+            <AuthInput icon={<Key/>} label="Enter your password" type="password"></AuthInput>
           </div>
        </div>
      )
