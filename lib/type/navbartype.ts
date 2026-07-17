0862bba00a74896042e2467639994fc92fcb4d5c
AungNyanHtet18 <aungnyanh81@gmail.com>
2026-07-17 21:28:25 +0630

    [Feat] company acccount can upload post

--- lib/type/navbartype.ts
+++ lib/type/navbartype.ts
@@ -6,14 +6,16 @@ export type NavbarType = {
 
 export const applicantNavbar: NavbarType[] = [
     {id: 1, name: "Applicant", url: "/applicant"},
-    {id: 2, name: "Network", url: "/network/postlist"},
+    {id: 2, name: "Community", url: "/network/postlist"},
     {id: 3, name: "Edit", url: "/applicant/edit"},
     {id: 4, name: "Joblist", url: "/applicant/joblist"}]
 
 export const companyNavbar: NavbarType[] = [
     {id: 1, name: "Company", url: "/companyaccount"},
     {id: 2, name: "Edit", url: "/companyaccount/edit"},
-    {id: 3, name: "Profile", url: "/companyaccount/detail"}]
+    {id: 3, name: "Community", url: "/network/postlist"},
+    {id: 4, name: "Profile", url: "/companyaccount/detail"}
+]
 
 export const companyAuthNavbar: NavbarType[] = [
     {id: 1, name: "Job", url: "/companyaccount/job/edit"}]
